d851fb7aba6bca2f4d63dafd433126c5a238c8bb
Joseph Magly <joseph.magly@gmail.com>
2021-05-15 12:01:03 -0400

    add starting example

---
+++ js/README.md
@@ -0,0 +1,2 @@
+# /js
+Distribution scripts, usually pulled down from teztools.io servers these can also be copied to the host of your choce and run from there. 
---
+++ js/dataload.js
@@ -0,0 +1,150 @@
+$(document).ready(function() {
+
+    const POLL_INTERVAL = 5000;
+  
+    const poll = ({ fn, validate, interval, maxAttempts }) => {
+        //console.log('Start poll...');
+  
+        const executePoll = async (resolve, reject) => {
+          //console.log('- poll');
+          const result = await fn();
+            setTimeout(executePoll, interval, resolve, reject);
+  
+        };
+  
+        return new Promise(executePoll);
+      };
+  
+      const getData = async () => {
+        fetch('https://api.teztools.io/token/prices.json')
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          appendData(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+  
+        return 0;
+      };
+  
+      const validateData = async (data) => {
+        return 0;
+      };
+  
+      const pollForData = poll({
+        fn: getData,
+        validate: validateData,
+        interval: POLL_INTERVAL,
+      })
+        .then(user => console.log(user))
+        .catch(err => console.error(err));
+  
+    function compareStrings(a, b) {
+        // Assuming you want case-insensitive comparison
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+  
+        return (a < b) ? -1 : (a > b) ? 1 : 0;
+    }
+  
+    function numberWithCommas(x) {
+      var parts = x.toString().split(".");
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return parts.join(".");
+  }
+  
+    function appendData(data) {
+      var htmlContent = '<div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">';
+      var contracts = data.contracts.sort(function(a, b) { return compareStrings(a.symbol, b.symbol)});
+
+      var singleTokens = document.getElementsByClassName('tt-token');
+      var singleToken = document.getElementById('token');
+  
+      for (var i = 0; i < contracts.length; i++) {
+          var contract = data.contracts[i];
+          var ratio = numberWithCommas(Math.round(((1 / contract.currentPrice) + Number.EPSILON) * 10000) / 10000);
+          var price = numberWithCommas(Math.round((contract.currentPrice + Number.EPSILON) * 10000000) / 10000000);
+  
+          var tokenAddress = `${contract.tokenAddress}_${contract.tokenId}`;
+          var tezpool = numberWithCommas(Math.round(contract.tezPool + Number.EPSILON));
+          var tokenpool = numberWithCommas(Math.round(contract.tokenPool + Number.EPSILON));
+
+          var content = 
+          `<div class="col">
+            <div class="card"> 
+                <div class="card-header pt-5 pb-5 text-center">
+                    <h5>${contract.symbol}</h5>
+                    <h5>${price} ꜩ</h5>
+                    <div class="mt-2">
+                        <medium>1 ꜩ gets <strong>${ratio}</strong> ${contract.symbol}</medium>
+                    </div>
+                    <div class="mt-2">
+                        <small>LP ${tezpool} ꜩ / ${tokenpool} ${contract.symbol}</small>
+                    </div>
+                </div>
+                <div class="card-body text-center">
+                    <div class="row">
+                        <div class="col-6 text-end">
+                            <a class="btn btn-sm btn-outline-success" href="https://quipuswap.com/?from=tez&amp;to=${tokenAddress}" target="_blank">BUY</a>
+                        </div>
+                        <div class="col-6 text-start">
+                            <a class="btn btn-sm btn-outline-danger" href="https://quipuswap.com/?from=${tokenAddress}&amp;to=tez" target="_blank">SELL</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>`
+
+          if (singleToken != undefined)
+          {
+            var tokenfq = singleToken.innerHTML.split(':');
+            var add = tokenfq[0];
+            var id = tokenfq[1];
+
+            if (add == contract.tokenAddress && id == contract.tokenId)
+            {
+                singleToken.innerHTML = content;
+            }
+          }
+
+          if (singleTokens != undefined)
+          {
+              for(let t of singleTokens) {
+                var tokenfq = t.innerHTML.split(':');
+                var add = tokenfq[0];
+                var id = tokenfq[1];
+  
+                if (add == contract.tokenAddress && id == contract.tokenId)
+                {
+                    t.innerHTML = content;
+                }
+              }
+          }
+  
+          htmlContent += content;
+      }
+  
+      htmlContent += '</div></div>';
+      var tokendata = document.getElementById('tokens');
+      tokendata.innerHTML = '';
+      tokendata.innerHTML = htmlContent;
+  
+      var blockData = document.getElementById('blockdata');
+      blockData.innerHTML = '';
+      var div = document.createElement('span');
+      var lastBlock = data.lastBlock.substr(data.lastBlock.length - 10)
+  
+      var date = new Date(data.lastBlockTime);
+      div.innerHTML = `...${lastBlock} found at ${date.toLocaleTimeString()}<br/>`;
+  
+      blockData.appendChild(div);
+    }
+  
+    window.setTimeout(function () {
+      window.location.reload();
+    }, 1800000);
+  });
+  
